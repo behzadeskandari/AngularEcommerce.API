feb005d177202a910aed9d47b90945b21a3068bc
behzad <behzad.b.i.g@gmail.com>
2023-09-11 18:55:50 +0330

    ShoppingCartItem and USer Roles Added

---
+++ Ecommerce.Common/Models/ShoppingCartItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Common.Models
+{
+    public class ShoppingCartItem : BaseEntity
+    {
+
+        public int Id { get; set; }
+
+        public List<Products> products { get; set; }
+
+        public int Amount { get; set; }
+
+        public string ShoppingCartId { get; set; }
+
+
+    }
+}
